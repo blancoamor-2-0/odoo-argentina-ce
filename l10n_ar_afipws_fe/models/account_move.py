9c20fe827f04a0dddbeda37b2157faa70991d020
Gabriela Rivero <regaby@gmail.com>
2025-04-11 20:31:48 -0300

    [UPD] RG. 5616/2024

--- l10n_ar_afipws_fe/models/account_move.py
+++ l10n_ar_afipws_fe/models/account_move.py
@@ -212,6 +212,7 @@ class AccountMove(models.Model):
                 inv.journal_id.get_pyafipws_last_invoice(inv.l10n_latam_document_type_id)['result']) + 1
 
             partner_id_code = commercial_partner.l10n_latam_identification_type_id.l10n_ar_afip_code
+            condicion_iva_receptor_id = commercial_partner.l10n_ar_afip_responsibility_type_id.code
             tipo_doc = partner_id_code or '99'
             nro_doc = \
                 partner_id_code and commercial_partner.vat or "0"
@@ -273,7 +274,7 @@ class AccountMove(models.Model):
                     imp_iva,
                     imp_trib, imp_op_ex, fecha_cbte, fecha_venc_pago,
                     fecha_serv_desde, fecha_serv_hasta,
-                    moneda_id, moneda_ctz
+                    moneda_id, moneda_ctz, cancela_misma_moneda_ext='N', condicion_iva_receptor_id=condicion_iva_receptor_id
                 )
             # elif afip_ws == 'wsmtxca':
             #     obs_generales = inv.comment
